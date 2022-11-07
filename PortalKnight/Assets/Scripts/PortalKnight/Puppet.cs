c5e0e7819bac6a5aa22ad076f9d27321ce56c74b
Thuleanx <thuleanx@tutanota.com>
2022-11-06 23:23:05 -0600

    health decrease on hit

--- PortalKnight/Assets/Scripts/PortalKnight/Puppet.cs
+++ PortalKnight/Assets/Scripts/PortalKnight/Puppet.cs
@@ -1,12 +1,20 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 using Thuleanx.Combat3D;
 
 namespace Thuleanx.PortalKnight {
 	[RequireComponent(typeof(Movable))]
 	public class Puppet : MonoBehaviour {
-		public Status 	Status {get; private set; }
+		public Status 	Status;
 		public Movable 	Movable {get; private set; }
+
+		[Space]
+		public UnityEvent OnDeath;
+		public UnityEvent OnHit;
+
+
+		public int InitialMaxHealth;
 
 		void Awake() {
 			Status = new Status(){
@@ -15,14 +23,23 @@ namespace Thuleanx.PortalKnight {
 			Movable = GetComponent<Movable>();
 		}
 
+		void OnEnable() {
+			Status.MaxHealth = InitialMaxHealth;
+			Status.Health = Status.MaxHealth;
+		}
+
 		void Start() {
 			foreach (var hurtbox in GetComponentsInChildren<Hurtbox3D>())
 				hurtbox.OnHit.AddListener(ProcessHit);
 		}
 
 		void ProcessHit(Hit3D hit) {
-			Movable.ApplyKnockback(hit.knockbackAmount * hit.hitDir);
-			Status.Health -= hit.damage;
+			if (!Status.IsDead) {
+				Movable.ApplyKnockback(hit.knockbackAmount * hit.hitDir);
+				Status.Health -= hit.damage;
+				if (Status.Health == 0) 	OnDeath?.Invoke();
+				else 						OnHit?.Invoke();
+			}
 		}
 	}
 }
--- PortalKnight/Assets/Scripts/PortalKnight/Status.cs
+++ PortalKnight/Assets/Scripts/PortalKnight/Status.cs
@@ -1,15 +1,20 @@
 using UnityEngine;
+using NaughtyAttributes;
 
 namespace Thuleanx.PortalKnight {
+	[System.Serializable]
 	public class Status {
-		public int MaxHealth;
 		public int Health {
 			get => _health; 
 			set => _health = Mathf.Clamp(value, 0, MaxHealth);
 		}
+		[ReadOnly] public int MaxHealth;
+
+
+		[SerializeField] 
+		[ProgressBar("health", 10)]
+		int _health;
 
 		public bool IsDead => Health == 0;
-
-		int _health;
 	}
 }
