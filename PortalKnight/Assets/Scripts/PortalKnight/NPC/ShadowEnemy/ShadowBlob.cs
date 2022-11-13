9f92f520eb14c16affe6f7aad18e18e3aad768c3
Thuleanx <101783970+An-Nguy@users.noreply.github.com>
2022-11-13 02:15:04 -0600

    blob

---
+++ PortalKnight/Assets/Scripts/PortalKnight/NPC/ShadowEnemy/ShadowBlob.cs
@@ -0,0 +1,39 @@
+using Thuleanx.Combat3D;
+using UnityEngine;
+using Thuleanx.Utils;
+
+namespace Thuleanx.PortalKnight {
+	public class ShadowBlob : MonoBehaviour, iHitGenerator3D {
+		public Hitbox3D Hitbox {get; private set; }
+
+		[SerializeField] int damage = 1;
+		[SerializeField, Range(0, 40)] float knockback = 8;
+		[SerializeField] GameObject bomb;
+		[SerializeField] float lifetime = 4;
+
+		Timer alive;
+
+		void Awake() {
+			Hitbox.HitGenerator = this;
+		}
+
+		void OnEnable() {
+			Hitbox.OnHit.AddListener(OnHit);
+			alive = lifetime;
+		}
+
+		void OnDisable() {
+			Hitbox.OnHit.RemoveListener(OnHit);
+		}
+
+		void Update() {
+			if (!alive) gameObject.SetActive(false);
+		}
+
+		void OnHit(Hit3D hit) {
+			gameObject.SetActive(false);
+		}
+
+		public Hit3D GenerateHit(Hitbox3D hitbox, Hurtbox3D hurtbox) => new Hit3D(damage, knockback, (hurtbox.transform.position - hitbox.transform.position).normalized, hurtbox.transform.position);
+	}
+}
