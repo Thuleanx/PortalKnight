1c5e47756e39f809e63629ac5ab880ec973aafdb
Thuleanx <101783970+An-Nguy@users.noreply.github.com>
2022-11-23 13:25:46 -0600

    health shine + increase camera ortho size

--- PortalKnight/Assets/Scripts/PortalKnight/UI/UIShine.cs
+++ PortalKnight/Assets/Scripts/PortalKnight/UI/UIShine.cs
@@ -5,23 +5,25 @@ using NaughtyAttributes;
 namespace Thuleanx.PortalKnight.UI {
 	public class UIShine : MonoBehaviour {
 		[SerializeField, Required] RectTransform shineWipe;
-		[SerializeField, Required] RectTransform shineCircle;
+		// [SerializeField, Required] RectTransform shineCircle;
 		[SerializeField, Range(0,1)] float shineWipeDuration;
-		[SerializeField, Range(0,1)] float shineCircleDuration;
-		[SerializeField] float shineDuration;
+		// [SerializeField, Range(0,1)] float shineCircleDuration;
+		[SerializeField, Range(0,3)] float shineRestDuration;
 		[SerializeField] float shineWipeDistance;
 
 		Sequence shineSequence;
 
 		void OnEnable() {
-			// shineSequence = DOTween.Sequence();
-			// shineSequence.AppendCallback(() => {
-			// 	shineCircle.localScale = Vector3.zero;
-			// });
-			// shineSequence.Append(shineWipe.anchoredPosition(Vector3.right * shineWipeDistance));
+			shineSequence = DOTween.Sequence();
+			shineSequence.AppendCallback(() => {
+				// shineCircle.localScale = Vector3.zero;
+				shineWipe.anchoredPosition = Vector3.right * -shineWipeDistance;
+			});
+			shineSequence.Append(shineWipe.DOAnchorPos(Vector3.right * shineWipeDistance, shineWipeDuration));
 			// shineSequence.AppendCallback(() => shineCircle.localScale = Vector3.one);
-			// shineSequence.Append(shineCircle.DOScale(Vector3.zero, shineCircleDuration));
-			// shineSequence.SetLoops(-1);
+			// shineSequence.Append(shineCircle.DOScale(Vector3.right, shineCircleDuration));
+			shineSequence.AppendInterval(shineRestDuration);
+			shineSequence.SetLoops(-1);
 		}
 
 		void OnDisable() {
