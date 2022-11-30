f3ac690559730f70ab490ea322dff499c7d550ec
Thuleanx <101783970+An-Nguy@users.noreply.github.com>
2022-11-30 11:01:39 -0600

    portal vfx

--- PortalKnight/Assets/Scripts/PortalKnight/Mapping/Passage.cs
+++ PortalKnight/Assets/Scripts/PortalKnight/Mapping/Passage.cs
@@ -1,17 +1,23 @@
 using UnityEngine;
+using UnityEngine.Events;
 using Thuleanx.Utils;
 using Thuleanx.PortalKnight.Interactions;
 using NaughtyAttributes;
+using Yarn.Unity;
 
 namespace Thuleanx.PortalKnight.Mapping {
 	[RequireComponent(typeof(Collider))]
 	public class Passage : Interactible {
 		public SceneTransitioner Transitioner { get; private set; }
 		[Required] public Passage Link;
+		public UnityEvent onEnable;
 
 		protected override void Interact() {
 			FindObjectOfType<PortalTransitioner>().Transition(this);
 		}
 
+
+		[YarnCommand("enable")]
+		void yarn_enable() => onEnable?.Invoke();
 	}
 }
