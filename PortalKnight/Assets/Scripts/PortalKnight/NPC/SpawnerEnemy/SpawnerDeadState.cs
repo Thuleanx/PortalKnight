23f6adaa58b97ef0ddd04189fd1200b8f1908515
Thuleanx <101783970+An-Nguy@users.noreply.github.com>
2022-11-30 18:57:16 -0600

    ending sound effect

--- PortalKnight/Assets/Scripts/PortalKnight/NPC/SpawnerEnemy/SpawnerDeadState.cs
+++ PortalKnight/Assets/Scripts/PortalKnight/NPC/SpawnerEnemy/SpawnerDeadState.cs
@@ -10,8 +10,10 @@ namespace Thuleanx.PortalKnight {
 			public override IEnumerator Coroutine(SpawnerEnemy spawner) {
 				foreach (var collider in spawner.GetComponentsInChildren<Collider>())
 					collider.enabled = false;
-				yield return new WaitForSeconds(2);
+				yield return new WaitForSeconds(1);
 				spawner.OnPermaDeath?.Invoke();
+				yield return new WaitForSeconds(2);
+				spawner.gameObject.SetActive(false);
 			}
 		}
 	}
